64d9b46f327415cb7c8de0d80ca33680ae6f2948
Nicolas Racchi <nicolas@racchi.com>
2023-03-05 19:24:37 +0100

    feat: working tests

--- test/CustomDispatcher.t.sol
+++ test/CustomDispatcher.t.sol
@@ -12,16 +12,33 @@ contract CustomDispatcherTest is Test {
         customDispatcher = HuffDeployer.deploy("CustomDispatcher");
     }
 
-    function testCanReachTheFirstFunction() public {
-        // first function is at offset 2 of 32 bytes
-        // so we need to shift right by 32 -2 = 30 bytes = 240 bits = 0xF0
+    function testCanReachFunction1() public {
+        // first function is at jumptable offset 2 of 32 bytes
+        // so we need to shift right by 32 - 2 = 30 bytes = 240 bits = 0xF0
         bytes memory data = abi.encodePacked(
-            uint256(0xf0000000000000000000000000000000000000000000000000000000)
+            uint256(
+                0xf000000000000000000000000000000000000000000000000000000000000000
+            )
         );
 
         (bool success, bytes memory testResult) = customDispatcher.call(data);
 
         assertEq(success, true);
-        assertEq(testResult, "Hello, world");
+        assertEq(testResult, abi.encode("hello from function 1"));
+    }
+
+    function testCanReachFunction2() public {
+        // second function is at jumptable offset 4 of 32 bytes
+        // so we need to shift right by 32 - 4 = 28 bytes = 224 bits = 0xE0
+        bytes memory data = abi.encodePacked(
+            uint256(
+                0xe000000000000000000000000000000000000000000000000000000000000000
+            )
+        );
+
+        (bool success, bytes memory testResult) = customDispatcher.call(data);
+
+        assertEq(success, true);
+        assertEq(testResult, abi.encode("hello from function 2"));
     }
 }
